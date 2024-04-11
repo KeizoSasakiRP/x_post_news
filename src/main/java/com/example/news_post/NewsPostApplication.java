a3bdecc3a894a57914d6f99245352dd4ce5f6169
Keizo Sasaki <keizo.sasaki@rakus-partners.co.jp>
2024-04-11 17:35:22 +0900

    Adding automatic posting function to X

--- src/main/java/com/example/news_post/NewsPostApplication.java
+++ src/main/java/com/example/news_post/NewsPostApplication.java
@@ -2,12 +2,14 @@ package com.example.news_post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NewsPostApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NewsPostApplication.class, args);
 	}
-
+	
 }
