530dc29d986b4949ac93246355b567f5ea743922
Keizo Sasaki <keizo.sasaki@rakus-partners.co.jp>
2024-04-11 11:22:12 +0900

    Initialize

---
+++ src/main/java/com/example/news_post/NewsPostApplication.java
@@ -0,0 +1,13 @@
+package com.example.news_post;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsPostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/news_post/NewsPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.news_post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsPostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
